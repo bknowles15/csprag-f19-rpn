805ed3d425f2c1f9602ef03e8db405a26894b517
bknowles <bknowles@umich.edu>
2019-10-25 14:52:35 -0400

    Added test for subtract and malformed input

--- rpn.py
+++ rpn.py
@@ -6,10 +6,10 @@ def do_operation(op, num1, num2):
     if op == '+':
         return num1 + num2
     if op == '-':
-        return num1 - num2
+        return num2 - num1
     if op == '*':
         return num1 * num2
-    return num1 / num2
+    return num2 / num1
 
 
 def calculate(arg):
@@ -17,11 +17,15 @@ def calculate(arg):
     stack = []
     for item in items:
         if item in OPS:
+            if len(stack) < 2:
+                raise TypeError('Malformed input')
             num1 = stack.pop()
             num2 = stack.pop()
             stack.append(do_operation(item, num1, num2))
         else:
             stack.append(int(item))
+    if len(stack) != 1:
+        raise TypeError('Malformed input')
     return stack.pop()
 
 
