2d7675e73bebcd971d7c8f641e5d0e85bf8d1897
bknowles <bknowles@umich.edu>
2019-10-25 14:33:53 -0400

    Implement addition

--- rpn.py
+++ rpn.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python3
 
+OPS = {'+', '-', '/', '*'}
+
+def do_operation(op, num1, num2):
+    if op == '+':
+        return num1 + num2
+    if op == '-':
+        return num1 - num2
+    if op == '*':
+        return num1 * num2
+    return num1 / num2
+
+
 def calculate(arg):
-    pass
+    items = arg.split()
+    stack = []
+    for item in items:
+        if item in OPS:
+            num1 = stack.pop()
+            num2 = stack.pop()
+            stack.append(do_operation(item, num1, num2))
+        else:
+            stack.append(int(item))
+    return stack.pop()
+
 
 def main():
     while True:
